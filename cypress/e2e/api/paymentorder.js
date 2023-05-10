0c603bec0d472e3e9f82fcf104585b3565c9263f
ethanbrian <mugoethan@gmail.com>
2023-05-10 16:04:16 +0300

    cypress-automation-framework -first commit

---
+++ cypress/e2e/api/paymentorder.js
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
+
+describe('Api Endpoints for zamupay', () => {
+  it('should send a payment order request with token', () => {
+
+    const id = uuidv4();
+    // Login to generate a token
+    cy.login();
+
+    // Data for the POST request
+const data = {
+  originatorConversationId: id,
+  paymentNotes: 'Transactions',
+  paymentOrderLines: [
+    {
+      recipient: {
+        name: "Anne E",
+        address: "Nairobi",
+        emailAddress: "anne.sagini@pycs.co.ke",
+        phoneNumber: "+254727363872",
+        idType: "National ID",
+        idNumber: "10038422",
+        financialInstitution: "Mpesa",
+        primaryAccountNumber: "+254790883713",
+        mccmnc: "63902",
+        ccy: 404,
+        country : "KE",
+        purpose: "TEST"
+      },
+      transaction: 
+      {
+        routeId: "10f4c83c-90b6-ed11-ad49-0ee00e897f52",
+        ChannelType: 20,
+        amount: 100,
+        reference: "Testing",
+        systemTraceAuditNumber:id
+      }
+
+    }
+  ]
+}
+    // Send the POST request with the token
+    cy.paymentOrderRequest(data);
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,74 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+  const clientId = Cypress.env('CLIENT_ID');
+  const clientSecret = Cypress.env('CLIENT_SECRET');
+  const scope = Cypress.env('SCOPE');
+
+  // Make the OAuth2 login request
+  cy.request({
+    method: 'POST',
+    url: 'https://auth.zamupay.com/connect/token',
+    form: true,
+    body: {
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret,
+      scope: scope,
+    },
+  }).then((response) => {
+    // Save the access token to the .env file
+    const token = response.body.access_token;
+    cy.writeFile('cypress/fixtures/oauth2_token.json', { token: token });
+  });
+});
+
+
+Cypress.Commands.add('paymentOrderRequest', (data) => {
+  // Read the token from the JSON file
+  cy.fixture('oauth2_token').then((token) => {
+    // Make the POST request with the token
+    cy.request({
+      method: 'POST',
+      url: 'https://sandboxapi.zamupay.com/v1/payment-order/new-order',
+      headers: {
+        Authorization: `Bearer ${token.token}`,
+      },
+      body: data,
+    }).then((response) => {
+      // Assert on the response status code
+      expect(response.status).to.equal(202);
+    });
+  });
+});
+
+
+
+
+  
+
+  
