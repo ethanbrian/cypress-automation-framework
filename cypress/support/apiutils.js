c52ea5ab91e489117c2fb30226700b54bc884a06
ethanbrian <mugoethan@gmail.com>
2023-05-18 10:19:20 +0300

    updated changes for getting transactional routes

--- cypress/support/apiutils.js
+++ cypress/support/apiutils.js
@@ -8,6 +8,8 @@ function performGetRequest(url, token) {
       Authorization: `Bearer ${token}`
     }
   }).then((response) => {
+       // Assert the status code
+       expect(response.status).to.eq(200);
     // Store the response in a JSON file in the fixtures folder
     fs.writeFile('cypress/fixtures/payment_order.json', JSON.stringify(response.body), (err) => {
       if (err) throw err;
