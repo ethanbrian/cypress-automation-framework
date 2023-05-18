a9223eef94c7a30008d02c2a472f68eb49cd0448
ethanbrian <mugoethan@gmail.com>
2023-05-18 10:20:56 +0300

    updated changes for getting transactional routes

--- cypress/e2e/api/TransactionRoutes.js
+++ cypress/e2e/api/TransactionRoutes.js
@@ -2,7 +2,7 @@
   const { performGetRequest } = require('../../support/apiutils');
 
 describe('Being able to retrieve transactional routes in zamupay', () => {
-  it('should perform a GET request and store the response', () => {
+  it('should perform a GET request ', () => {
     cy.getToken().then((token) => {
       const url = 'https://sandboxapi.zamupay.com/v1/transaction-routes/assigned-routes';
       performGetRequest(url, token);
